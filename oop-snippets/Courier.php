d9b421a536e8ecda71fc08f88146398e039e834b
usman-tahir <tahir.usman.ali94@outlook.com>
2016-10-19 17:54:16 -0400

    toString() method for the Courier class; example of serializing /de-serializing an object

--- oop-snippets/Courier.php
+++ oop-snippets/Courier.php
@@ -6,6 +6,7 @@ class Courier {
   public $name;
   public $homeCountry;
   
+  /* Old code before using serialize()
   public function __construct($name) {
     $this->name = $name;
     return true;
@@ -34,7 +35,52 @@ class Courier {
     return $courierList;
   }
   
+  public function __toString() {
+    return $this->name . ' (' . $this->homeCountry . ')<br>';
+  }
+  */
+  
+  public function __construct($name, $homeCountry) {
+    $this->name = $name;
+    $this->homeCountry = $homeCountry;
+    $this->logFile = $this->getLogFile();
+    return true;
+  }
+  
+  protected function getLogFile() {
+    return fopen('/tmp/error_log.txt', 'a');
+  }
+  
+  protected function log($message) {
+    if ($this->logFile) {
+      fputs($this->logFile, "Log message: " . $message . "\n");
+    }
+  }
+  
+  public function __sleep() {
+    // Only store the safe properties
+    return array("name", "homeCountry");
+  }
+  
+  public function __wakeup() {
+    // Properties are restored; now add the logFile
+    $this->logFile = $this->getLogFile();
+    return true;
+  }
+  
+  public function __toString() {
+    return $this->name . ' (' . $this->homeCountry . ')<br>';
+  }
+  
 }
 
 $courier = new \Shipping\Courier("Sample Courier");
-echo var_dump($courier);
+$courier->homeCountry = 'Peru';
+echo $courier;
+$courier->log("Hello World!");
+/*
+// Serialized data can be stored into a database (since it is a text-based format)
+echo serialize($courier);
+
+// echo var_dump($courier);
+*/
