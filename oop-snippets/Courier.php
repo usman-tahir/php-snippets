e1bf12857d18c2f716a70a64da364b51324705f7
usman-tahir <tahir.usman.ali94@outlook.com>
2016-10-19 15:20:08 -0400

    added a sample static method, and var_dump() results for a Courier object

--- oop-snippets/Courier.php
+++ oop-snippets/Courier.php
@@ -14,4 +14,20 @@ class Courier {
     return true;
   }
   
+  public static function getCouriersByCountry($country) {
+    // Get a list of couriers with their homeCountry = $country
+    
+    // Create a Courier object for each result
+    
+    // Return an array of the results
+    return $courierList;
+  }
+  
 }
+
+$mono = new Courier("Monospace Delivery");
+echo var_dump($mono);
+echo $mono->name;
+
+// Example usage of static method
+$spanishCouriers = Courier::getCouriersByName('Spain');
