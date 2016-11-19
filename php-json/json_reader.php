e6e48db6ce6a128ab0208447847752a864e08522
usman-tahir <tahir.usman.ali94@outlook.com>
2016-11-19 12:09:25 -0500

    added a foreach loop for the colorsArray object

--- php-json/json_reader.php
+++ php-json/json_reader.php
@@ -1,9 +1,14 @@
 <?php
 
 $jsonContents = file_get_contents("./data.json");
+/*
 if ($jsonContents) {
   echo "TRUE<br>";
   echo var_dump($jsonContents);
 }
+*/
 $jsonArray = json_decode($jsonContents, true);
-print_r($jsonArray);
+// print_r($jsonArray["colorsArray"]);
+foreach ($jsonArray["colorsArray"][0] as $key => $value) {
+  echo "The hex code for <span style='color: {$value}'>{$key}</span> is {$value}<br>";
+}
